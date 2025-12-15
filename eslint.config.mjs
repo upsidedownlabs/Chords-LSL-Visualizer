d01125985dfdce7b4d575634576668da44e1be49
Ritika Mishra <ritikamis@gmail.com>
2025-12-15 12:50:22 +0530

    use direct import for ESLint config instead of FlatCompat

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,5 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import nextVitals from 'eslint-config-next/core-web-vitals';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [...compat.extends('next/core-web-vitals')];
-
-export default eslintConfig;
+export default [
+  ...nextVitals,
+];
